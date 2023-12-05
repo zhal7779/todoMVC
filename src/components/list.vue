94b9fe9311c3e63116787cc212009c0a9ab47c2e
Minyoung Lee <zhal7779@gmail.com>
2023-12-06 00:06:14 +0900

    Style: 체크된 label 스타일 적용

--- src/components/list.vue
+++ src/components/list.vue
@@ -5,8 +5,6 @@ import { storeToRefs } from 'pinia';
 const store = useStore();
 const { todoList } = storeToRefs(store);
 const { checkedList, removeList } = store;
-
-console.log(todoList);
 </script>
 
 <template>
@@ -14,8 +12,14 @@ console.log(todoList);
     <ul>
       <li v-for="(list, index) in todoList" :key="index">
         <div class="list">
-          <input type="checkbox" @click="checkedList(list.text)" />
-          <label>{{ list.text }}</label>
+          <input
+            type="checkbox"
+            class="toggle"
+            @click="checkedList(list.text)"
+          />
+          <label :class="list.checked ? 'cheked-list' : 'unchecked-list'">{{
+            list.text
+          }}</label>
           <button @click="removeList(list.text)">삭제</button>
         </div>
       </li>
@@ -31,6 +35,20 @@ div {
   margin-top: 2rem;
 }
 .list {
+  font-size: 24px;
   text-align: start;
+}
+.list > label {
+  padding: 15px 15px 15px 60px;
+}
+.toggle {
+  width: 40px;
+}
+.unchecked-list {
+  color: #4d4d4d;
+}
+.cheked-list {
+  color: #d9d9d9;
+  text-decoration: line-through;
 }
 </style>
