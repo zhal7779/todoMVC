95afc716ad8f2ba4b5932c84e8236cbea1af319d
minyoung <zhal7779@naver.com>
2023-12-05 13:51:39 +0900

    Init: first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header>
+    <h1>todos</h1>
+  </header>
+  <main>
+    <TheWelcome />
+  </main>
+  <footer></footer>
+</template>
+
+<style scoped>
+body {
+  background-color: #f5f5f5;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1>HelloWorld</h1>
+</template>
